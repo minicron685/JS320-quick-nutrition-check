bf04ec80caec2f1a55d2546196c677e9809ab159
Eddie Lee <inrez00@uw.edu>
2021-06-08 17:42:35 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # quick-nutrition-check
+
+This app is a simple nutrition information calculator that allows users to obtain basic nutrition facts for the types and amounts of ingredients they enter.
+
+• API key is required from Edamam to use the app. (https://developer.edamam.com/)
+
+• Please save APP_ID and APP_KEY from Edamam in "/src/components/Ingredient.js". 
+
+(example)
+
+const APP_ID = "1a1111a1"
+const APP_KEY = "111111aa11aaaaaa1111aa111111a111"
+
+
--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import Ingredient from "./components/Ingredient";
+import NutritionResult from "./components/NutritionResult";
+import History from "./components/History";
+
 function App() {
+  const [total, setTotal] = useState(null)
   return (
-    <div>hello</div>
+    <div className="container">
+      <h1 style={{marginBottom: 40}}>Quick Nutrition Check</h1>
+      <h3>Enter ingredient and measurement (U.S. system, e.g., 8 oz beef)</h3>
+      <p>Input your ingredient(s):</p>
+
+      <Ingredient data={(info) => setTotal(info)} />
+      <hr />
+      <NutritionResult data={total} />
+      <hr />
+      <History />
+
+    </div>
   );
 }
 
